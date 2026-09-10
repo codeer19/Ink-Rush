1d10397f07d59b6bd7d6d1a6692bce538f2bb8b7
codeer19 <adityaverma19200@gmail.com>
2026-09-11 00:26:10 +0530

    fix: connect deployed frontend to socket server

--- src/socket.js
+++ src/socket.js
@@ -1,13 +1,25 @@
 import { io } from "socket.io-client";
 
-
 const SERVER_URL =
     import.meta.env.VITE_SERVER_URL ||
-    "http://localhost:3001";
+    "https://ink-rush.onrender.com/";
 
+console.log("Connecting to backend:", SERVER_URL);
 
-const socket =
-    io(SERVER_URL);
+const socket = io(SERVER_URL);
 
+socket.on("connect", () => {
+    console.log(
+        "Socket connected:",
+        socket.id
+    );
+});
+
+socket.on("connect_error", (error) => {
+    console.error(
+        "Socket connection failed:",
+        error.message
+    );
+});
 
 export default socket;
